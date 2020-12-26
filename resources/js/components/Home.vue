e38fe5006f31ad8a74d090cd193faf0f1045a608
yogithesymbian <yogirenbox33@gmail.com>
2020-12-26 09:13:03 +0800

    init

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card-header">Vue JS SPA</div>
+
+				<div class="card-body">
+					<h2>{{ welcome }}</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "home",
+		computed: {
+			welcome() {
+				return this.$store.getters.welcomeMessage;
+			},
+		},
+	};
+</script>
---
+++ resources/js/store.js
@@ -0,0 +1,12 @@
+export default {
+    state: {
+        welcomeMessage: "Welcome to my vue app"
+    },
+    getters: {
+        welcomeMessage(state) {
+            return state.welcomeMessage;
+        }
+    },
+    mutations: {},
+    actions: {}
+};
