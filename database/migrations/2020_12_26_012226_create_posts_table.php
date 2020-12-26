c4f485e04fd6b327bc11d8bc7f39f47971183c45
yogithesymbian <yogirenbox33@gmail.com>
2020-12-26 12:43:09 +0800

    delete [1]

--- database/migrations/2020_12_26_012226_create_posts_table.php
+++ database/migrations/2020_12_26_012226_create_posts_table.php
@@ -14,9 +14,9 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
-            $table->text('content');
+            $table->text('body');
             $table->timestamps();
         });
     }
