d0dd6cfaae79c1711308cb6d0a38630dbe10cf19
yogithesymbian <yogirenbox33@gmail.com>
2020-12-28 11:25:57 +0800

    fix bug [1]

--- resources/js/utils/store.js
+++ resources/js/utils/store.js
@@ -1,10 +1,10 @@
 export default {
     state: {
-        sh_brg_masuk: "/barang/masuk/",
-        input_brg_masuk: "/barang/masuk/input/",
-        update_brg_masuk: "/barang/masuk/update/",
-        delete_brg_masuk: "/barang/masuk/delete/",
-        find_brg_masuk: "/barang/masuk/find/"
+        sh_brg_masuk: "/barang/masuk",
+        input_brg_masuk: "/barang/masuk/input",
+        update_brg_masuk: "/barang/masuk/update",
+        delete_brg_masuk: "/barang/masuk/delete",
+        find_brg_masuk: "/barang/masuk/find"
     },
     getters: {},
     mutations: {},
