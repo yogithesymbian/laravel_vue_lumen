6e5e015dfe2e698fdb6bfaee1c1260b11e429744
yogithesymbian <yogirenbox33@gmail.com>
2020-12-28 09:59:41 +0800

    cors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require("lodash");
+// window._ = require("lodash");
 
 // try {
 //     window.$ = window.jQuery = require("jquery");
@@ -6,9 +6,9 @@ window._ = require("lodash");
 //     require("bootstrap");
 // } catch (e) {}
 
-window.axios = require("axios");
+// window.axios = require("axios");
 
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+// window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 // let token = document.head.querySelector('meta[name="csrf-token"');
 
